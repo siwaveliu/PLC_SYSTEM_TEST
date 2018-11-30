2df4a91460513b39591cb3240b247641b3e98dcd
bq.liu <bq.liu@siwave.com.cn>
2018-11-30 17:13:45 +0800

    -m "修改usb_test，初始化和创建移到while外面"

--- lib/usb_test.py
+++ lib/usb_test.py
@@ -1,18 +1,18 @@
 import usb_relay
 import time
 
+i = 0
 usb = usb_relay.UsbRelay()
 usb.init_device(None)
-# usb.close(1,2,3,4,5,6)
-usb.open(1,2,3,4,5,6)
-print("power off all of meters and cco, delay 5s")
-time.sleep(3)
-usb.close(1,2,3,4,5,6)
-print("power on all of meters and cco. delay 5s for boot")
-time.sleep(3)
-usb.open(1,2,3,4,5,6)
-print("power off all of meters and cco, delay 5s")
-time.sleep(3)
-usb.close(1,2,3,4,5,6)
-print("power on all of meters and cco. delay 5s for boot")
+while True:
+    i += 1
+    print("the [" + str(i) + '] times')
+    # usb.close(1,2,3,4,5,6)
+    usb.open(5)
+    print("power off all of meters and cco, delay 5s")
+    time.sleep(3)
+    usb.close(5)
+    print("power on all of meters and cco. delay 5s for boot")
+    time.sleep(5)
+
 usb = None
